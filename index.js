56aa6e2d3398a782b5d2021892fa9e93446083fd
savannahgrasmick <grasmicksavannah@gmail.com>
2018-06-20 20:44:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ let currentUser = "Savannah";
 let welcomeMessage = `Welcome to Klossybook, ${currentUser}!`
 
 let excitedWelcomeMessage = `WELCOME TO KLOSSYBOOK, ${currentUser.toUpperCase()}!`;
+
+let shortGreeting = ""
